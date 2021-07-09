c0e8d3ef9e3b611a5771d396c20f926e881d081e
Beatryx <baba.beubeu@gmail.com>
2021-07-09 13:05:30 +0200

    update the project

--- imageprocess/test.jl
+++ imageprocess/test.jl
@@ -1,7 +1,16 @@
+using Images, Statistics, ImageBinarization, ImageEdgeDetection
 begin
-    using Images
-    cactus = load("imageprocess\\IMG_3514.JPG")
-    tinyCactus = shrink_images(cactus, 6)
+    cactus = red.(load("imageprocess\\IMG_3514.JPG"))
+    #tinyCactus = shrink_image(cactus, 6)
 end
 
-cactus[1000:2000,1000:3000]
+corners = fastcorners(cactus, 10, 0.1)
+img_copy = RGB.(cactus)
+img_copy[corners] .= RGB(1.0, 0.0, 0.0)
+img_copy
+
+edge = detect_edges(cactus, Canny(spatial_scale = 2.5, low = ImageEdgeDetection.Percentile(5), high = ImageEdgeDetection.Percentile(90)))
+
+mosaic(cactus, img_copy, edge, nrow=1)
+
+save("imageprocess\\test.png",mosaic(cactus, img_copy, edge, nrow=3))
